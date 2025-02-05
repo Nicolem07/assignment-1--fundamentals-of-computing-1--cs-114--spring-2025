eb15e1e821eb6e865f1922b2e709531fa650c18a
Nicolem07 <157421652+Nicolem07@users.noreply.github.com>
2025-02-04 21:44:09 -0600

    Making of Initials

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,17 @@
 // Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+  Scanner scan = new Scanner(System.in);
+  scan.close();
+  System.out.println("NNN\t\t\tNNN\t\tMMM\t\t\t\tMMM");
+  System.out.println("NNNN\t\tNNN\t\tMMMM\t\t\tMMM");
+  System.out.println("NNNNN\t\tNNN\t\tMMMMM\t\t  MMMMM");
+  System.out.println("NNN\tNNN\t\tNNN\t\tMMM\tMM\t\t MM MMM");
+  System.out.println("NNN\t NNNN\tNNN\t\tMMM\t MM\t\tMM\tMMM");
+  System.out.println("NNN\t\tNNN\tNNN\t\tMMM\t  MMMMMMM\tMMM");
+  System.out.println("NNN\t\t NN\tNNN\t\tMMM\t\tMMM\t\tMMM");
+
   }
 }
