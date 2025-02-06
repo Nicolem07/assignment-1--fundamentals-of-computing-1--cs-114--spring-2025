feec5f9eec2cc7347e245f437166c9857b6616dd
Nicolem07 <157421652+Nicolem07@users.noreply.github.com>
2025-02-06 17:56:10 -0600

    I finished the letters and everything else

--- Assignment1.java
+++ Assignment1.java
@@ -1,17 +1,36 @@
-// Import libraries here
 import java.util.Scanner;
+import java.util.Random;
 
 public class Assignment1 {
-  public static void main(String[] args) {
-  Scanner scan = new Scanner(System.in);
-  scan.close();
-  System.out.println("NNN\t\t\tNNN\t\tMMM\t\t\t\tMMM");
-  System.out.println("NNNN\t\tNNN\t\tMMMM\t\t\tMMM");
-  System.out.println("NNNNN\t\tNNN\t\tMMMMM\t\t  MMMMM");
-  System.out.println("NNN\tNNN\t\tNNN\t\tMMM\tMM\t\t MM MMM");
-  System.out.println("NNN\t NNNN\tNNN\t\tMMM\t MM\t\tMM\tMMM");
-  System.out.println("NNN\t\tNNN\tNNN\t\tMMM\t  MMMMMMM\tMMM");
-  System.out.println("NNN\t\t NN\tNNN\t\tMMM\t\tMMM\t\tMMM");
-
-  }
+	public static void main(String[] args) {
+		double temp;
+		double celsius;
+		String result = "";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("NNN       NNN  MMM           MMM");
+		System.out.println("NNNN      NNN  MMMM         MMMM");
+		System.out.println("NNNNN     NNN  MMMMM       MMMMM");
+		System.out.println("NNN NNN   NNN  MMM MM     MM MMM");
+		System.out.println("NNN NNNN  NNN  MMM  MM   MM  MMM");
+		System.out.println("NNN  NNNN NNN  MMM  MMMMMMM  MMM");
+		System.out.println("NNN   NNN NNN  MMM    MMM    MMM");
+		System.out.println("");
+		System.out.println("Please enter a number in Fahrenheit:");
+		temp = scan.nextDouble();
+		celsius = ((temp - 32) * 5/9);
+		scan.nextLine();
+		System.out.println("");
+		System.out.println("Please enter a 5-character string:");
+		String fivechar = scan.nextLine();
+		if (fivechar.length() == 5) {
+			result = "" + fivechar.charAt(3) + fivechar.charAt(2) + fivechar.charAt(1);
+		}
+		System.out.println("");
+		System.out.println("Generating random number. Continuing");
+		System.out.println("");
+		Random generator = new Random();
+		int randomnum = generator.nextInt(Integer.MAX_VALUE);
+		System.out.println("Your new string is " + celsius + result + randomnum);
+		scan.close();
+	}
 }
